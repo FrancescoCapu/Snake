9eb69796daf272bd878fa58699c6b3c0ea08f458
dibon.francesco03@gmail.com <dibon.francesco03@gmail.com>
2020-11-17 18:45:37 +0100

    trasferello

--- frmSnake.cs
+++ frmSnake.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Snake
 {
-    public partial class Form1 : Form
+    public partial class frmSnake : Form
     {
-        public Form1()
+        public frmSnake()
         {
             InitializeComponent();
+        }
+
+        private void frmSnake_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Trasferello");
         }
     }
 }
